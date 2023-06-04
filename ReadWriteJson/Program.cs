bd69cbc40e2000ebb12a32b25c5a7fee76631cf3
R-Tatara <rabumel.dpl@gmail.com>
2023-06-05 00:18:36 +0900

    Add ReadWriteJson

---
+++ ReadWriteJson/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using JsonFileIO.Jsons;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigJson config = new ConfigJson
+            {
+                network = new NetworkJson
+                {
+                    ipAddr = "192.168.0.20",
+                    port = 8888
+                },
+                filepath = @".\log"
+            };
+
+            //Serialize from object to json
+            var jsonWriteData = JsonConvert.SerializeObject(config, Formatting.Indented);
+
+            //Write JSON file
+            using (var sw = new StreamWriter(@".\sample.json", false, System.Text.Encoding.UTF8)) //Overwrite mode
+            {
+                sw.Write(jsonWriteData);
+            }
+
+            //Read JSON file
+            using (var sr = new StreamReader(@".\sample.json", System.Text.Encoding.UTF8))
+            {
+                var jsonReadData = sr.ReadToEnd();
+
+                //Deserialize from json to object
+                config = JsonConvert.DeserializeObject<ConfigJson>(jsonReadData);
+            }
+
+            Console.WriteLine(config.network.ipAddr);
+            Console.WriteLine(config.network.port);
+            Console.WriteLine(config.filepath);
+        }
+    }
+}
