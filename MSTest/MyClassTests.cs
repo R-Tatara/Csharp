55575cb1c1eac5f2547ab72cd23446cd01101fc1
R-Tatara <rabumel.dpl@gmail.com>
2023-06-11 02:03:33 +0900

    Add MSTest sample

---
+++ MSTest/MyClassTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Tests
+{
+    [TestClass()]
+    public class MyClassTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            MyClass obj = new MyClass();
+            var answer = obj.Sum(1, 2);
+            Assert.AreEqual(expected: 3, actual: answer);
+        }
+    }
+}
---
+++ MSTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class MyClass
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            int a = 1;
+            int b = 2;
+            int result;
+
+            result = obj.Sum(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
