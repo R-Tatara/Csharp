6a3d26021c5234aca96965a940dff00cbb825255
R-Tatara <rabumel.dpl@gmail.com>
2023-06-17 19:45:15 +0900

    Add AsynchronousProcessing

---
+++ AsynchronousProcessing/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("button1_Click start");
+            int ret = await Task.Run( () => Task1Async() );
+            Debug.WriteLine("button1_Click end");
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("button2_Click start");
+            int ret = await Task.Run( () => Task2Async() );
+            Debug.WriteLine("button2_Click end");
+        }
+
+        private async Task<int> Task1Async()
+        {
+            Debug.WriteLine("Task1Async start");
+            await Task.Delay(2000);
+            Debug.WriteLine("Task1Async end");
+            return 0;
+        }
+
+        private async Task<int> Task2Async()
+        {
+            Debug.WriteLine("Task2Async start");
+            await Task.Delay(2000);
+            Debug.WriteLine("Task2Async end");
+            return 0;
+        }
+    }
+}
